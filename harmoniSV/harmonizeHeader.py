bf54fa6e2829cb8b2ba3e8f94827d5b3d4daabf2
HanC <caohan7659@gmail.com>
2023-10-02 18:42:34 +0800

    [ADD]: more annotations on the script

--- harmoniSV/harmonizeHeader.py
+++ harmoniSV/harmonizeHeader.py
@@ -15,13 +15,13 @@ parser = argparse.ArgumentParser(prog="harmonisv harmonize-header",
                                  description="Harmonize VCF headers",
                                  add_help=False)
 
-required = parser.add_argument_group("required arguments")
-parser.add_argument("-i", "--invcf", required=False, type=str,
-                    help="Comma-separated list of input VCF files. Duplicate headers will use the first one.")
-parser.add_argument("-o", "--output", required=True, type=str,
+io_arg = parser.add_argument_group("Input/Output arguments")
+io_arg.add_argument("-i", "--invcf", required=False, type=str,
+                    help="Comma-separated list of input VCF files. Duplicate headers will use the first one, including SAMPLE header. For multi-sample VCF, please make sure all input VCFs have the same SAMPLE order.")
+io_arg.add_argument("-f", "--file-list", required=False, type=str,
+                    help="File containing a list of input VCF files, one VCF per line. VCFs from both -i and -f will be used.")
+io_arg.add_argument("-o", "--output", required=True, type=str,
                     help="Output VCF header file")
-parser.add_argument("-f", "--file-list", required=False, type=str,
-                    help="File containing a list of input VCF files, one VCF per line")
 
 optional = parser.add_argument_group("optional arguments")
 optional.add_argument("-r", "--ref-vcf", required=False, type=str,
@@ -33,8 +33,8 @@ class UniqueHeader:
     """Class to store unique VCF headers"""
     def __init__(self) -> None:
         self.header = []
-        self.id_dict = {'_GENERIC': []}
-        self.header_dict = {'_GENERIC': []}
+        self.id_dict = {'_GENERIC': []} # key: header key, value: list of IDs
+        self.header_dict = {'_GENERIC': []} # key: header key, value: list of header lines
 
 
     def add_record(self, record: pysam.VariantHeaderRecord) -> None:
@@ -42,15 +42,18 @@ class UniqueHeader:
         record_key = record.key
         record_id = record.get('ID')
 
-        # unstructured header
+        # generic headers, no header key
         if record_id is None:
             if record_key not in self.id_dict['_GENERIC']:
                 self.id_dict['_GENERIC'].append(record_key)
                 self.header_dict['_GENERIC'].append(str(record))
+        # INFO, FORMAT, FILTER, ALT, etc.
         else:
+            # add new key (e.g., INFO, FORMAT)
             if record_key not in self.id_dict:
                 self.id_dict[record_key] = []
                 self.header_dict[record_key] = []
+            # add new header line
             if record_id not in self.id_dict[record_key]:
                 self.id_dict[record_key].append(record_id)
                 self.header_dict[record_key].append(str(record))
@@ -64,8 +67,10 @@ class UniqueHeader:
 
     def get_header(self) -> list:
         """Get unique header"""
+        # add generic headers first
         if len(self.id_dict['_GENERIC']) > 0:
             self.header.extend(self.header_dict['_GENERIC'])
+        # add other headers
         for key in self.header_dict:
             if key != '_GENERIC':
                 self.header.extend(self.header_dict[key])
@@ -96,7 +101,8 @@ def harmonizeHeader_main(cmdargs) -> None:
     invcf_list = read_files(args)
 
     # add headers
-    sample_list = []
+    sample_list = [] # list of samples shown in the last line
+    # first add reference if exist
     if args.ref_vcf is not None:
         logger.info(f"Reading reference VCF file: {args.ref_vcf}")
         ref_vcf = pysam.VariantFile(args.ref_vcf)
@@ -104,7 +110,7 @@ def harmonizeHeader_main(cmdargs) -> None:
         sample_list.extend(list(ref_vcf.header.samples))
         ref_vcf.close()
         
-
+    # add all VCFs
     for vcf_file in invcf_list:
         if args.ref_vcf is not None:
             if vcf_file == args.ref_vcf:
@@ -112,6 +118,7 @@ def harmonizeHeader_main(cmdargs) -> None:
         logger.info(f"Reading input VCF file: {vcf_file}")
         vcf = pysam.VariantFile(vcf_file)
         new_header.add_header(vcf.header)
+        # add samples
         if len(sample_list) == 0:
             sample_list.extend(list(vcf.header.samples))
         vcf.close()
