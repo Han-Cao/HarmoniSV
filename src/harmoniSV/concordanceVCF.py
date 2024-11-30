996a1530c16366195adf9c22456ecbc50130a22d
HanC <caohan7659@gmail.com>
2024-11-30 18:03:57 +0800

    [UPD]: compare by vector to speed up

--- src/harmoniSV/concordanceVCF.py
+++ src/harmoniSV/concordanceVCF.py
@@ -8,6 +8,7 @@ import argparse
 import logging
 
 import pysam
+import numpy as np
 
 from utils import read_vcf, parse_cmdargs
 
@@ -30,47 +31,20 @@ optional_arg.add_argument('--compare', metavar="alleles", type=str, default="all
 optional_arg.add_argument("-h", "--help", action='help', help='show this help message and exit')
 
 
-class WeightedGC:
-    """ Weighted genotype concordance """
-    def __init__(self) -> None:
-        self.gc_lst = [[], [], []]
-    
-
-    def add(self, gc_code: int):
-        """ 
-        1 or -1 : 0/0 concordance or not
-        2 or -2 : 0/1 concordance or not
-        3 or -3 : 1/1 concordance or not
-        """
-        if gc_code is None:
-            return
-        res_con = gc_code > 0
-        self.gc_lst[abs(gc_code) - 1].append(res_con)
-    
-
-    def concordance(self) -> float:
-        """ Calculate weighted concordance """
-        gc_1 = sum(self.gc_lst[0]) / len(self.gc_lst[0]) if len(self.gc_lst[0]) > 0 else 1
-        gc_2 = sum(self.gc_lst[1]) / len(self.gc_lst[1]) if len(self.gc_lst[1]) > 0 else 1
-        gc_3 = sum(self.gc_lst[2]) / len(self.gc_lst[2]) if len(self.gc_lst[2]) > 0 else 1
-        return (gc_1 + gc_2 + gc_3) / 3
-
-
 class ConcordanceCounter:
     """Accumulator to store concordance statistics"""
     def __init__(self):
         self.total = 0
         self.concordant = 0
 
-    def add(self, n: float):
+    def add(self, x: float):
         """Add concordance statistics"""
         self.total += 1
-        self.concordant += n
+        self.concordant += x
 
     def rate(self) -> float:
         """Calculate concordance rate"""
-        return self.concordant / self.total
-
+        return float(self.concordant / self.total)
 
 
 GT_DICT = {(0, 0): 0,
@@ -78,66 +52,60 @@ GT_DICT = {(0, 0): 0,
            (1, 0): 1,
            (1, 1): 2}
 
-def gt_concordance(gt_in: tuple, gt_ref: tuple) -> tuple:
-    """Compare genotype concordance of two genotypes, return concordance by genotype, existence, and weighted concordance code"""
-    
-    # if any reference genotype is missing, return None and skip
-    if None in gt_ref or None in gt_in:
-        return (None, None, None)
-    ac_ref = GT_DICT[gt_ref]
 
-    # # if any input genotype is missing, return 0 for both
-    # if None in gt_in:
-    #     return (0, 0, -ac_ref - 1)
-    
-    ac_in = GT_DICT[gt_in]
-    # concordance by genotype
-    if ac_in == ac_ref:
-        return (1, 1, ac_ref + 1)
-    # concordance by existence only
-    elif (ac_in > 0) and (ac_ref > 0):
-        return (0, 1, -ac_ref - 1)
-    # discordance for both
+def gt2code(gt: tuple) -> int:
+    """ Convert gentoype to code for comparison """
+    if None in gt:
+        return -1
     else:
-        return (0, 0, -ac_ref - 1)
+        return GT_DICT[gt]
 
 
 def variant_concordance(invar: pysam.VariantRecord, 
                         refvar: pysam.VariantRecord, 
-                        sample_map: dict,
+                        in_samples: list,
+                        ref_samples: list,
                         counter_gt: ConcordanceCounter,
                         counter_exist: ConcordanceCounter,
                         counter_weighted: ConcordanceCounter) -> tuple:
-    """Compare genotype concordance of two variants, return concordance by genotype and existence (i.e., 0/1 = 1/1)"""
-    con_gt = 0
-    con_exist = 0
-    n_sample = len(sample_map)
-    con_weighted = WeightedGC()
+    """ Compare genotype concordance of two variants across samples """
 
-    for sample1, sample2 in sample_map.items():
-        gt_in = invar.samples[sample1]['GT']
-        gt_ref = refvar.samples[sample2]['GT']
-        con_gt_i, con_exist_i, gc_code_i = gt_concordance(gt_in, gt_ref)
+    # vectorize genotypes
+    invar_gt = [gt2code(invar.samples[sample]['GT']) for sample in in_samples]
+    refvar_gt = [gt2code(refvar.samples[sample]['GT']) for sample in ref_samples]
 
-        # if input / reference genotype is missing, skip
-        if con_gt_i is None:
-            n_sample -= 1
-            continue
+    invar_gt = np.array(invar_gt, dtype=np.int8)
+    refvar_gt = np.array(refvar_gt, dtype=np.int8)
 
-        con_gt += con_gt_i
-        con_exist += con_exist_i
-        con_weighted.add(gc_code_i)
+    # find non-missing genotypes in both VCF
+    idx_cmp = (invar_gt != -1) & (refvar_gt != -1)
 
-        counter_gt.add(con_gt_i)
-        counter_exist.add(con_exist_i)
-    
-    # if no sample can be compared, return None
-    if n_sample == 0:
+    if np.sum(idx_cmp) == 0:
         return (None, None, None)
-    else:
-        wgc = con_weighted.concordance()
-        counter_weighted.add(wgc)
-        return (con_gt / n_sample, con_exist / n_sample, wgc)
+
+    invar_cmp = invar_gt[idx_cmp]
+    refvar_cmp = refvar_gt[idx_cmp]
+
+    # concordance by genotype
+    con_gt = invar_cmp == refvar_cmp
+    gc = np.mean(con_gt)
+    # concordance by existence
+    con_exist = (invar_cmp > 0) == (refvar_cmp > 0)
+    gc_exist = np.mean(con_exist)
+    # weighted genotype concordance
+    idx_0 = refvar_cmp == 0
+    idx_1 = refvar_cmp == 1
+    idx_2 = refvar_cmp == 2
+    gc_0 = np.mean(con_gt[idx_0]) if np.sum(idx_0) > 0 else 1
+    gc_1 = np.mean(con_gt[idx_1]) if np.sum(idx_1) > 0 else 1
+    gc_2 = np.mean(con_gt[idx_2]) if np.sum(idx_2) > 0 else 1
+    gc_weighted = (gc_0 + gc_1 + gc_2) / 3
+
+    counter_gt.add(gc)
+    counter_exist.add(gc_exist)
+    counter_weighted.add(gc_weighted)
+
+    return float(gc), float(gc_exist), float(gc_weighted)
 
 
 def find_variants(vcf:pysam.VariantFile, contig: str, pos: int) -> list:
@@ -146,17 +114,24 @@ def find_variants(vcf:pysam.VariantFile, contig: str, pos: int) -> list:
     var_lst = []
 
     # start = pos - 1
-    for variant in vcf.fetch(contig=contig, start=pos-1):
+    for variant in vcf.fetch(contig=contig, start=pos-1, stop=pos):
         if variant.pos == pos:
+            assert len(variant.alleles) == 2
             var_lst.append(variant)
-        elif variant.pos > pos:
-            break
     
     return var_lst
 
 
-def process_pos(prev_var: pysam.VariantRecord, invar_lst: list, refvcf: pysam.VariantFile, outvcf: pysam.VariantFile, compare: str,
-                sample_map: dict, counter_gt: ConcordanceCounter, counter_exist: ConcordanceCounter, counter_weighted: ConcordanceCounter) -> None:
+def process_pos(prev_var: pysam.VariantRecord, 
+                invar_lst: list, 
+                refvcf: pysam.VariantFile, 
+                outvcf: pysam.VariantFile, 
+                compare: str,
+                in_samples: list,
+                ref_samples: list,
+                counter_gt: ConcordanceCounter, 
+                counter_exist: ConcordanceCounter,
+                counter_weighted: ConcordanceCounter) -> None:
     """ Process all variants at the same position """
 
     # only 1 variant at the previous position:
@@ -175,7 +150,13 @@ def process_pos(prev_var: pysam.VariantRecord, invar_lst: list, refvcf: pysam.Va
         invar_key = invar.alleles if compare == "alleles" else invar.id
         if invar_key in refvar_map:
             refvar = refvar_map[invar_key]
-            con_gt, con_exist, con_weighted = variant_concordance(invar, refvar, sample_map, counter_gt, counter_exist, counter_weighted)
+            con_gt, con_exist, con_weighted = variant_concordance(invar, 
+                                                                  refvar, 
+                                                                  in_samples, 
+                                                                  ref_samples, 
+                                                                  counter_gt, 
+                                                                  counter_exist, 
+                                                                  counter_weighted)
         else:
             con_gt = None
             con_exist = None
@@ -196,14 +177,18 @@ def add_header(header: pysam.VariantHeader) -> pysam.VariantHeader:
     return header
 
 
-def read_map(mapfile: str) -> dict:
+def read_map(mapfile: str) -> tuple:
     """Read sample map file"""
-    sample_map = {}
+
+    in_samples = []
+    ref_samples = []
     with open(mapfile, 'r') as f:
         for line in f:
             sample1, sample2 = line.strip().split('\t')
-            sample_map[sample1] = sample2
-    return sample_map
+            in_samples.append(sample1)
+            ref_samples.append(sample2)
+
+    return in_samples, ref_samples
 
 
 def concordanceVCF_main(cmdargs):
@@ -229,30 +214,29 @@ def concordanceVCF_main(cmdargs):
 
     # get sample map
     if args.map is not None:
-        sample_map = read_map(args.map)
+        in_samples, ref_samples = read_map(args.map)
     else:
         sample_invcf = list(invcf.header.samples)
         sample_refvcf = list(refvcf.header.samples)
-        sample_overlap = [x for x in sample_invcf if x in sample_refvcf]
-        sample_map = dict(zip(sample_overlap, sample_overlap))
+        in_samples = [x for x in sample_invcf if x in sample_refvcf]
+        ref_samples = in_samples
     
     # check key
     if args.compare not in ['alleles', 'id']:
         raise ValueError(f'--compare must be either "alleles" or "id"')
     
-    logger.info(f'Find {len(sample_map)} samples in both VCF files to compare.')
+    logger.info(f'Find {len(in_samples)} samples in both VCF files to compare.')
 
     # loop through all variants of invcf
-    verbosity = 2000
+    verbosity = 10000
     prev_var = None
     invar_working = []
     for i, cur_var in enumerate(invcf.fetch()):
         if i % verbosity == 0:
             logger.info(f"Processing variant at {cur_var.chrom}:{cur_var.pos} ({cur_var.id})...")
         
-        # make input is biallelic
-        if len(cur_var.alleles) > 2:
-            raise ValueError("Input VCF is not biallelic")
+        # make sure input is biallelic
+        assert len(cur_var.alleles) == 2
 
         # get the first variant
         if prev_var is None:
@@ -266,11 +250,11 @@ def concordanceVCF_main(cmdargs):
             invar_working.append(cur_var)
         # seen all variants at the pos, process
         elif cur_var.pos > prev_var.pos:
-            process_pos(prev_var, invar_working, refvcf, outvcf, args.compare, sample_map, counter_gt, counter_exist, counter_weighted)
+            process_pos(prev_var, invar_working, refvcf, outvcf, args.compare, in_samples, ref_samples, counter_gt, counter_exist, counter_weighted)
             invar_working = []
         # switch to a new chromosome
         elif cur_var.chrom != prev_var.chrom:
-            process_pos(prev_var, invar_working, refvcf, outvcf, args.compare, sample_map, counter_gt, counter_exist, counter_weighted)
+            process_pos(prev_var, invar_working, refvcf, outvcf, args.compare, in_samples, ref_samples, counter_gt, counter_exist, counter_weighted)
             invar_working = []
         else:
             raise ValueError("Input VCF is not sorted by position")
@@ -278,12 +262,13 @@ def concordanceVCF_main(cmdargs):
         prev_var = cur_var.copy()
     
     # process the last position
-    process_pos(prev_var, invar_working, refvcf, outvcf, args.compare, sample_map, counter_gt, counter_exist, counter_weighted)
+    process_pos(prev_var, invar_working, refvcf, outvcf, args.compare, in_samples, ref_samples, counter_gt, counter_exist, counter_weighted)
    
     # summarise concordance results
-    logger.info(f'Overall concordance of genotypes: {counter_gt.rate()}')
-    logger.info(f'Overall concordance of existence: {counter_exist.rate()}')
-    logger.info(f'Overall weighted genotype concordance: {counter_weighted.rate()}')
+    logger.info(f'Compared {counter_gt.total} variants')
+    logger.info(f'Average genotype concordance: {counter_gt.rate()}')
+    logger.info(f'Average existence concordance: {counter_exist.rate()}')
+    logger.info(f'Average weighted genotype concordance: {counter_weighted.rate()}')
     
     # close files
     invcf.close()
